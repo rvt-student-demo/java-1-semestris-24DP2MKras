8b464ab90bea47b7ee7b741fd991b45f51b7729c
Māris Krastiņš <A240364MK@rvt.lv>
2025-10-19 14:11:45 +0300

    java mooc 3-2

--- src/main/java/rvt/javamooc32.java
+++ src/main/java/rvt/javamooc32.java
@@ -2,7 +2,7 @@ package rvt;
     import java.util.*;
 public class javamooc32 {
     public static void main(String[] args) {
-        listsize();
+        removeLast();
     }
     public static void onlythesenumbers() {
         Scanner reader = new Scanner(System.in);
@@ -49,11 +49,32 @@ public class javamooc32 {
         System.out.println();
         System.out.print("Search for? ");
         String search = reader.nextLine();
-        for(int i = 0; i <= list.size(); i++) {
-            if() {
-                System.out.println(list(i) + "was found!");
-            }
+        for(int i = 0; i < list.size(); i++) {
+            if(list.get(i).equals(search)) {
+                System.out.println(list.get(i) + " was found!");
+            } 
         }
+    }
+    public static void removeLast(ArrayList<String> strings) {
+        if(!strings.isEmpty()) {
+            strings.remove(strings.size() - 1);
+        }
+    }
+    
+    public static void removeLast(){
+        ArrayList<String> strings = new ArrayList<>();
+
+        strings.add("First");
+        strings.add("Second");
+        strings.add("Third");
+        
+        System.out.println(strings);
+
+        removeLast(strings);
+        removeLast(strings);
+        
+        System.out.println(strings);
+
     }
         
     
